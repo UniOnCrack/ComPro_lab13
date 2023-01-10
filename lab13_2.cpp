14851518057675322e66436e7e07b6c0884c514b
uni <ni.nitid@gmail.com>
2023-01-10 15:26:59 +0700

    lab13_2 completed.

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 using namespace std;
 
+long int gcd(long int,long int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+long int gcd(long int x, long int y){
+    int z;
+    if(x%y == 0){
+        z = y;
+    }else{
+        z = gcd(y, x%y);
+    }
+    return z;
 }
